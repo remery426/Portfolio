1b9a851124d0ed2f092be989004e44dfef526b11
remery426 <emeryryan1@gmail.com>
2018-02-23 09:00:33 -0800

    changed projects layout and made images safe for ie

--- src/Components/Projects/Card.js
+++ src/Components/Projects/Card.js
@@ -7,16 +7,17 @@ const Card = (props) =>{
               <div class="card">
                 <h2> {props.title} </h2>
                 <div class="card-image">
-                  <img src={props.imgUrl} alt="Image temporarily unavailable"/>
+                  <img style = {{maxHeight: 400, maxWidth:500 }} src={props.imgUrl} alt="Image temporarily unavailable"/>
+                </div>
+
+                <div class="card-action">
+                  <a href= {props.linkUrl}>{props.linkDescription}</a>
                 </div>
                 <div class="card-content" >
 
                   <p>Tools Utilized: {props.tools}</p>
 
                   <p>  About: {props.content}</p>
-                </div>
-                <div class="card-action">
-                  <a href= {props.linkUrl}>{props.linkDescription}</a>
                 </div>
               </div>
           </div>
